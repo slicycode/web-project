01f20d8261c925a9531675b6fa8383c75b36780e
slicycode <deparisjules180@gmail.com>
2021-12-06 14:52:59 +0100

    test pour pull

--- templates/candidature.tpl
+++ templates/candidature.tpl
@@ -3,16 +3,85 @@
 {block name=body}
 <div class="app">
     <h1>Ma candidature</h1>
-    <div class="candidature">
-        <div class="candidature__img">
-            <img src="https://images.unsplash.com/photo-1638309024787-1bd16766f123?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80">
-        </div>
+    <form class="form" action="register" method="post">
+    <div class="form__input">
+        <label for="stacked-name">Nom du groupe</label>
+        <input autofocus="autofocus" name="nom_groupe" value="{$valeurs.nom_groupe|escape|default:''}" type="text" id="stacked-name" placeholder="Nom du groupe" />
+        <span class="erreur"> {$messages.nom_groupe|escape|default:''}</span>
 
-        <div class="candidature__desc">
-            <h2>TITRE</h2>
-            <p>audio</p>
-            <p>DESCRIPTION</p>
-        </div>
     </div>
+    <div class="form__input">
+        <label for="stacked-name">Département</label>
+        <input autofocus="autofocus" name="id_dep" value="{$valeurs.id_dep|escape|default:''}" type="text" id="stacked-name" placeholder="Département" />
+        <span class="erreur"> {$messages.id_dep|escape|default:''}</span>
+
+    </div>
+    <div class="form__input">
+        <label for="stacked-name">Scène</label>
+        <input name="id_scene" value="{$valeurs.id_scene|escape|default:''}"  type="text" id="stacked-name" placeholder="Scène" />
+        <span class="erreur"> {$messages.id_scene|escape|default:''}</span>
+
+    </div>
+     <div class="form__input">
+        <label for="stacked-foo">Style</label>
+        <input name="style" value="{$valeurs.style|escape|default:''}"  type="text" id="stacked-foo" placeholder="Scène" />
+        <span class="erreur"> {$messages.style|escape|default:''}</span>
+
+    </div>
+
+    <div class="form__input">
+        <label for="stacked-foo">Représentant du groupe</label>
+        <input name="id_rep" type="text" id="stacked-foo" placeholder="Représentant du groupe" value="{$valeurs.style|escape|default:''}"/>
+        <span class="erreur"> {$messages.id_rep|escape|default:''}</span>
+
+    </div>
+    <div class="form__input">
+        <label for="stacked-foo">Année de création</label>
+        <input name="annee" value="{$valeurs.annee|escape|default:''}" type="text" id="stacked-foo" placeholder="Année de création" />
+        <span class="erreur"> {$messages.annee|escape|default:''}</span>
+    </div>
+    
+    <div class="form__input">
+        <label for="stacked-foo">Présentation</label>
+        <input name="presentation" value="{$valeurs.presentation|escape|default:''}" type="text" id="stacked-foo" placeholder="Présentation" />
+        <span class="erreur"> {$messages.presentation|escape|default:''}</span>
+    </div>
+
+    <div class="form__input">
+        <label for="stacked-foo">Expérience</label>
+        <input name="experience" value="{$valeurs.experience|escape|default:''}" type="text" id="stacked-foo" placeholder="Expérience" />
+        <span class="erreur"> {$messages.experience|escape|default:''}</span>
+    </div>
+
+    <div class="form__input">
+        <label for="stacked-foo">Site ou page Facebook</label>
+        <input name="site" value="{$valeurs.site|escape|default:''}" type="text" id="stacked-foo" placeholder="Site ou page Facebook" />
+        <span class="erreur"> {$messages.site|escape|default:''}</span>
+    </div>
+
+    <div class="form__input">
+        <label for="stacked-foo">Youtube</label>
+        <input name="youtube" value="{$valeurs.youtube|escape|default:''}" type="text" id="stacked-foo" placeholder="Chaîne Youtube" />
+        <span class="erreur"> {$messages.youtube|escape|default:''}</span>
+    </div>
+
+    <div class="form__input">
+        <label for="stacked-foo">Status associatif</label>
+        <input name="assos" value="{$valeurs.assos|escape|default:''}" type="text" id="stacked-foo" placeholder="Status associatif" />
+        <span class="erreur"> {$messages.assos|escape|default:''}</span>
+    </div>
+
+    <div class="form__input">
+        <label for="stacked-foo">SACEM</label>
+        <input name="assos" value="{$valeurs.assos|escape|default:''}" type="text" id="stacked-foo" placeholder="Status associatif" />
+        <span class="erreur"> {$messages.assos|escape|default:''}</span>
+    </div>
+
+
+
+    <div class="pure-controls">
+        <button type="submit" class="pure-button pure-button-primary">S'inscrire</button>
+    </div>
+</form>
 </div>
 {/block}
--- templates_c/a9c53f24f84ae9ab8f406f15a3bee2a6fb1f62c6_0.file.candidature.tpl.php
+++ templates_c/a9c53f24f84ae9ab8f406f15a3bee2a6fb1f62c6_0.file.candidature.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.34-dev-7, created on 2021-12-02 11:14:58
+/* Smarty version 3.1.34-dev-7, created on 2021-12-06 13:42:00
   from 'C:\laragon\www\tps\projet\templates\candidature.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.34-dev-7',
-  'unifunc' => 'content_61a8aab29725f1_20429898',
+  'unifunc' => 'content_61ae13287d0b67_47648576',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'a9c53f24f84ae9ab8f406f15a3bee2a6fb1f62c6' => 
     array (
       0 => 'C:\\laragon\\www\\tps\\projet\\templates\\candidature.tpl',
-      1 => 1638443688,
+      1 => 1638798118,
       2 => 'file',
     ),
   ),
@@ -20,26 +20,26 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_61a8aab29725f1_20429898 (Smarty_Internal_Template $_smarty_tpl) {
+function content_61ae13287d0b67_47648576 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_loadInheritance();
 $_smarty_tpl->inheritance->init($_smarty_tpl, true);
 ?>
 
 <?php 
-$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_212239238961a8aab2965ca6_36519993', 'title');
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_164308912261ae1328543461_34133240', 'title');
 ?>
 
 <?php 
-$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_2064291361a8aab296e835_94103443', 'body');
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_20950342661ae132854a7a2_25612689', 'body');
 $_smarty_tpl->inheritance->endChild($_smarty_tpl, 'layout.tpl');
 }
 /* {block 'title'} */
-class Block_212239238961a8aab2965ca6_36519993 extends Smarty_Internal_Block
+class Block_164308912261ae1328543461_34133240 extends Smarty_Internal_Block
 {
 public $subBlocks = array (
   'title' => 
   array (
-    0 => 'Block_212239238961a8aab2965ca6_36519993',
+    0 => 'Block_164308912261ae1328543461_34133240',
   ),
 );
 public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
@@ -49,12 +49,12 @@ Candidature<?php
 }
 /* {/block 'title'} */
 /* {block 'body'} */
-class Block_2064291361a8aab296e835_94103443 extends Smarty_Internal_Block
+class Block_20950342661ae132854a7a2_25612689 extends Smarty_Internal_Block
 {
 public $subBlocks = array (
   'body' => 
   array (
-    0 => 'Block_2064291361a8aab296e835_94103443',
+    0 => 'Block_20950342661ae132854a7a2_25612689',
   ),
 );
 public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
@@ -62,17 +62,67 @@ public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
 
 <div class="app">
     <h1>Ma candidature</h1>
-    <div class="candidature">
-        <div class="candidature__img">
-            <img src="https://images.unsplash.com/photo-1638309024787-1bd16766f123?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80">
-        </div>
+    <form class="form" action="register" method="post">
+    <div class="form__input">
+        <label for="stacked-name">Nom</label>
+        <input autofocus="autofocus" name="nom" value="<?php echo (($tmp = @htmlspecialchars($_smarty_tpl->tpl_vars['valeurs']->value['nom'], ENT_QUOTES, 'UTF-8', true))===null||$tmp==='' ? '' : $tmp);?>
+" type="text" id="stacked-name" placeholder="Nom" />
+        <span class="erreur"> <?php echo (($tmp = @htmlspecialchars($_smarty_tpl->tpl_vars['messages']->value['nom'], ENT_QUOTES, 'UTF-8', true))===null||$tmp==='' ? '' : $tmp);?>
+</span>
 
-        <div class="candidature__desc">
-            <h2>TITRE</h2>
-            <p>audio</p>
-            <p>DESCRIPTION</p>
-        </div>
     </div>
+    <div class="form__input">
+        <label for="stacked-name">Prénom</label>
+        <input autofocus="autofocus" name="prenom" value="<?php echo (($tmp = @htmlspecialchars($_smarty_tpl->tpl_vars['valeurs']->value['prenom'], ENT_QUOTES, 'UTF-8', true))===null||$tmp==='' ? '' : $tmp);?>
+" type="text" id="stacked-name" placeholder="Prénom" />
+        <span class="erreur"> <?php echo (($tmp = @htmlspecialchars($_smarty_tpl->tpl_vars['messages']->value['prenom'], ENT_QUOTES, 'UTF-8', true))===null||$tmp==='' ? '' : $tmp);?>
+</span>
+
+    </div>
+    <div class="form__input">
+        <label for="stacked-name">Adresse</label>
+        <input name="adresse" value="<?php echo (($tmp = @htmlspecialchars($_smarty_tpl->tpl_vars['valeurs']->value['adresse'], ENT_QUOTES, 'UTF-8', true))===null||$tmp==='' ? '' : $tmp);?>
+"  type="text" id="stacked-name" placeholder="Adresse" />
+        <span class="erreur"> <?php echo (($tmp = @htmlspecialchars($_smarty_tpl->tpl_vars['messages']->value['adresse'], ENT_QUOTES, 'UTF-8', true))===null||$tmp==='' ? '' : $tmp);?>
+</span>
+
+    </div>
+     <div class="form__input">
+        <label for="stacked-foo">Code postal</label>
+        <input name="codepostal" value="<?php echo (($tmp = @htmlspecialchars($_smarty_tpl->tpl_vars['valeurs']->value['codepostal'], ENT_QUOTES, 'UTF-8', true))===null||$tmp==='' ? '' : $tmp);?>
+"  type="text" id="stacked-foo" placeholder="Code postal" />
+        <span class="erreur"> <?php echo (($tmp = @htmlspecialchars($_smarty_tpl->tpl_vars['messages']->value['codepostal'], ENT_QUOTES, 'UTF-8', true))===null||$tmp==='' ? '' : $tmp);?>
+</span>
+
+    </div>
+
+    <div class="form__input">
+        <label for="stacked-password">Mot de passe</label>
+        <input name="password" type="password" id="stacked-password" placeholder="Mot de passe" />
+        <span class="erreur"> <?php echo (($tmp = @htmlspecialchars($_smarty_tpl->tpl_vars['messages']->value['password'], ENT_QUOTES, 'UTF-8', true))===null||$tmp==='' ? '' : $tmp);?>
+</span>
+
+    </div>
+    <div class="form__input">
+        <label for="stacked-email">Email</label>
+        <input name="email" value="<?php echo (($tmp = @htmlspecialchars($_smarty_tpl->tpl_vars['valeurs']->value['email'], ENT_QUOTES, 'UTF-8', true))===null||$tmp==='' ? '' : $tmp);?>
+" type="email" id="stacked-email" placeholder="Email" />
+        <span class="erreur"> <?php echo (($tmp = @htmlspecialchars($_smarty_tpl->tpl_vars['messages']->value['email'], ENT_QUOTES, 'UTF-8', true))===null||$tmp==='' ? '' : $tmp);?>
+</span>
+    </div>
+    
+    <div class="form__input">
+        <label for="stacked-phone">Téléphone</label>
+        <input name="telephone" value="<?php echo (($tmp = @htmlspecialchars($_smarty_tpl->tpl_vars['valeurs']->value['telephone'], ENT_QUOTES, 'UTF-8', true))===null||$tmp==='' ? '' : $tmp);?>
+" type="telephone" id="stacked-phone" placeholder="Téléphone" />
+        <span class="erreur"> <?php echo (($tmp = @htmlspecialchars($_smarty_tpl->tpl_vars['messages']->value['telephone'], ENT_QUOTES, 'UTF-8', true))===null||$tmp==='' ? '' : $tmp);?>
+</span>
+    </div>
+
+    <div class="pure-controls">
+        <button type="submit" class="pure-button pure-button-primary">S'inscrire</button>
+    </div>
+</form>
 </div>
 <?php
 }
